51fff767e634e95f0093a09f3017b85d73e8ce93
Adrian Bardan <abardandev@gmail.com>
2026-01-04 06:48:54 -0700

    added more user fields and user session handling

--- dogecoiner-db/SProcs/UpsertUsers.sql
+++ dogecoiner-db/SProcs/UpsertUsers.sql
@@ -6,29 +6,45 @@ MERGEs user data
 */
 MERGE INTO Users AS t
 USING @Users AS s
-ON 
-    s.UserId = t.UserId
+ON
+    s.Email = t.Email
 
 WHEN NOT MATCHED BY TARGET THEN
 INSERT
 (
-    Username,
-    IsRegistered
+    Email,
+    FirstName,
+    LastName,
+    Picture,
+    Sub,
+    ProviderSub,
+    ProviderName
 )
 VALUES
 (
-    s.Username,
-    s.IsRegistered
+    s.Email,
+    s.FirstName,
+    s.LastName,
+    s.Picture,
+    s.Sub,
+    s.ProviderSub,
+    s.ProviderName
 )
-    
-WHEN MATCHED THEN 
-UPDATE SET
-    t.Username = s.Username,
-    t.IsRegistered = s.IsRegistered
 
-OUTPUT 
-    $action, 
+WHEN MATCHED THEN
+UPDATE SET
+    t.Email = s.Email,
+    t.FirstName = s.FirstName,
+    t.LastName = s.LastName,
+    t.Picture = s.Picture,
+    t.Sub = s.Sub,
+    t.ProviderSub = s.ProviderSub,
+    t.ProviderName = s.ProviderName,
+    t.ModifiedUtc = GETUTCDATE()
+
+OUTPUT
+    $action,
     INSERTED.UserId,
-    INSERTED.Username;
+    INSERTED.Email;
 
 RETURN 0
